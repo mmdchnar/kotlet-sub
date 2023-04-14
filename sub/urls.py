fc8ec8bd1815218ae769bb1acb9476f7d78b6a56
mmdchnar <mmdchenar@gmail.com>
2023-04-15 01:37:25 +0330

    change slug to str for ignore "=" character

--- sub/urls.py
+++ sub/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('sub/<slug:slug>', views.sub, name='sub'),
-    path('create/<slug:slug>', views.create, name='create'),
-    path('delete/<slug:slug>', views.delete, name='delete'),
-    path('rename/<slug:slug>', views.rename, name='rename'),
-    path('uuid/<slug:slug>', views.uuid, name='uuid'),
+    path('sub/<str:slug>', views.sub, name='sub'),
+    path('create/<str:slug>', views.create, name='create'),
+    path('delete/<str:slug>', views.delete, name='delete'),
+    path('rename/<str:slug>', views.rename, name='rename'),
+    path('uuid/<str:slug>', views.uuid, name='uuid'),
 ]
