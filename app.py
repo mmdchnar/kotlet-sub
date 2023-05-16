c0f2359d4bbd1feb2891f1ee38791cbb75343472
Mohammad Chenarani <115184760+mmdchnar@users.noreply.github.com>
2023-05-16 03:46:58 +0330

    Add try/except for invalid urls

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ INBOUND_ID = os.environ.get("INBOUND_ID")
 SNI = os.environ.get("SNI")
 SID = os.environ.get("SID")
 PBK = os.environ.get("PBK")
-URI = os.environ.get("URI")\
-    .replace('$SNI$', SNI)\
-    .replace('$PBK$', PBK)\
-    .replace('$SID$', SID)\
+URI = os.environ.get("URI") \
+    .replace('$SNI$', SNI) \
+    .replace('$PBK$', PBK) \
+    .replace('$SID$', SID) \
     .replace('$PORT$', PORT)
 PANEL_URL = os.environ.get("PANEL_URL")
 API_PATH = os.environ.get("API_PATH")
@@ -26,7 +26,6 @@ DOMAINS = dict(zip(DOMAINS_keys.split(','), DOMAINS_values.split(',')))
 
 USERNAME = os.environ.get("USER")
 PASSWORD = os.environ.get("PASS")
-
 
 app = Flask(__name__)
 
@@ -40,45 +39,48 @@ def index():
 def sub(base64_uuid):
     response = 'Kotlet was Eaten by Dogs!! :)'
 
-    uuid = b64decode(base64_uuid.encode()).decode()
+    try:
+        uuid = b64decode(base64_uuid.encode()).decode()
 
-    with session() as s:
-        s.post(f'{PANEL_URL}/login',
-               data={
-                   'username': USERNAME,
-                   'password': PASSWORD,
-               })
+        with session() as s:
+            s.post(f'{PANEL_URL}/login',
+                   data={
+                       'username': USERNAME,
+                       'password': PASSWORD,
+                   })
 
-        req = s.get(f'{PANEL_URL}{API_PATH}/get/{INBOUND_ID}')
-        clients = loads(req.json()['obj']['settings'])['clients']
-        for client in clients:
-            if client['id'] == uuid:
-                user = s.get(f"{PANEL_URL}{API_PATH}/getClientTraffics/{client['email']}").json()['obj']
-                status = client['enable'] and user['enable']
-                status = '🟢' if status else '🔴'
-                if user['total']:
-                    total = (user['total'] - (user['up'] + user['down'])) / 1073741824
-                    total = round(total, 2)
-                else:
-                    total = '∞'
-                if user['expiryTime']:
-                    expiry = (datetime.fromtimestamp(user['expiryTime'] // 1000) - datetime.now()).days
-                else:
-                    expiry = '∞'
-                uris = {}
-                for domain in DOMAINS:
-                    uri = URI
-                    uri = uri.replace('$UUID$', uuid)
-                    uri = uri.replace('$DOMAIN$', DOMAINS[domain])
-                    uri = uri.replace('$NAME$',
-                                      f"{status} ({domain}) {total} GB | {expiry} Days | "
-                                      f"{client['email'][4:]} {client['email'][:3]}")
-                    uris[domain] = uri
+            req = s.get(f'{PANEL_URL}{API_PATH}/get/{INBOUND_ID}')
+            clients = loads(req.json()['obj']['settings'])['clients']
+            for client in clients:
+                if client['id'] == uuid:
+                    user = s.get(f"{PANEL_URL}{API_PATH}/getClientTraffics/{client['email']}").json()['obj']
+                    status = client['enable'] and user['enable']
+                    status = '🟢' if status else '🔴'
+                    if user['total']:
+                        total = (user['total'] - (user['up'] + user['down'])) / 1073741824
+                        total = round(total, 2)
+                    else:
+                        total = '∞'
+                    if user['expiryTime']:
+                        expiry = (datetime.fromtimestamp(user['expiryTime'] // 1000) - datetime.now()).days
+                    else:
+                        expiry = '∞'
+                    uris = {}
+                    for domain in DOMAINS:
+                        uri = URI
+                        uri = uri.replace('$UUID$', uuid)
+                        uri = uri.replace('$DOMAIN$', DOMAINS[domain])
+                        uri = uri.replace('$NAME$',
+                                          f"{status} ({domain}) {total} GB | {expiry} Days | "
+                                          f"{client['email'][4:]} {client['email'][:3]}")
+                        uris[domain] = uri
 
-                response = ''
-                for i in uris:
-                    response += uris[i] + '\n\n'
-                response = b64encode(response.encode()).decode()
+                    response = ''
+                    for i in uris:
+                        response += uris[i] + '\n\n'
+                    response = b64encode(response.encode()).decode()
+    except Exception as error:
+        print('Error from My Exception: ', error)
 
     return response
 
